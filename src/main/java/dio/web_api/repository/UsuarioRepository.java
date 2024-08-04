9e960f1b8d6d10e5bd61272bd76e173582a68915
Andersondlz <andersondlz2009@gmail.com>
2024-08-04 19:34:33 -0300

    commit de estudos finalizando o domingo com a dio

--- src/main/java/dio/web_api/repository/UsuarioRepository.java
+++ src/main/java/dio/web_api/repository/UsuarioRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import dio.web_api.handler.BusinessException;
 import dio.web_api.handler.CampoObrigatorioException;
 import dio.web_api.model.Usuario;
 
